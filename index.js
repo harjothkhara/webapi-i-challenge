a1a235ba3ceca3fb58d45b316e1a99932f620375
harjothkhara <harjoth.khara@gmail.com>
2019-05-28 15:14:01 -0700

    put requests returns all of the modified object, error message added to each request type, put request will cancel if not enough info is included, post req now sends back entire created object

--- index.js
+++ index.js
@@ -69,46 +69,64 @@ server.get('/api/users/:id', (req, res) => {
         res.status(200).json(user);
     })
     .catch(err => {
-        res.status(500).json({ });
+        res.status(404).json({message: 'The user with the specified ID does not exist.' });
     })
 })
 
 server.post('/api/users', (req, res) => {
-    db.insert(req.body)
-    .then(user => {
-        res.status(200).json(user);
+    if (!req.body.bio || !req.body.name) {
+        res.status(400).json({ message: 'Please provide name and bio for the user.'})
+    }
+    else {
+        db.insert(req.body)
+    .then(edited => {
+        db.findById(edited.id)
+        .then(user => {
+            res.status(201).json(user);
+        })
     })
     .catch(err => {
-        res.status(500).json({ });
+        res.status(500).json({ message: 'There was an error while saving the user to the database'})
     })
+    }
 })
 
-server.delete('/api/users/id', (req, res) => {
+server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
-    db.remove(id)
-    .then(deleted => {
-        res.status(204).end;
+    db.findById(id)
+    .then(user => {
+        db.remove(id)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({message: 'The user could not be removed'})
+        })
     })
-    .catch(err => {
-        res.status(500).json({ });
-    })
+    .catch(err => res.status(404).json({message: 'The user with the specified ID does not exist.'}))
 })
 
 server.put('/api/users/:id', (req, res) => {
     const { id } = req.params;
-    const userInfo = req.body
-    db.update(id, userInfo)
+    const userInfo = req.body;
+    if (!userInfo.name || !userInfo.bio) {
+        res.status(400).json({ message: 'Please provide name and bio for the user.'})
+    }
+    else {
+        db.update(id, userInfo)
     .then(updated => {
-        if (updated) {
-            res.status(200).json(updated)
-        } else {
-            res.status(404).json({ message: "User not found"})
-        }
-       
+        db.findById(id)
+            .then(user => {
+                res.status(200).json(user);
+            })
+            .catch(err => {
+                res.status(404).json({ message: 'The user with the specified ID does not exist.'})
+            })
     })
     .catch(err => {
-
+        res.status(500).json({ message: 'The user information could not be modified.'})
     })
+    }
 })
 
 // 5. tell our server to listen for connections on a particular prompt
